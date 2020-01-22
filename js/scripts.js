30380f3183cc2541a28270ddde2981186b7b846f
Jamison Cozart and Zakkrey Short <zakkrey@>
2020-01-22 08:31:09 -0800

    add if/else statement, place for inside function.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
     event.preventDefault();
     var countTo = parseInt($("#countTo").val());
     var countBy = parseInt($("#countBy").val());
-    for(var index = 0; index <= countTo; index += countBy) {
-      $("#output").append("<li>" + index + "</li>");
+    if(countBy > countTo) {
+      $("#output").append("<li>" + "Error: count to needs to be greater than count by." + "</li>")
+    } else {
+      for(var index = 0; index <= countTo; index += countBy) {
+        $("#output").append("<li>" + index + "</li>");
+      }
     }
+      
   })
 })
